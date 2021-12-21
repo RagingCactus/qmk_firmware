7457c434a057979558c346732dbb2b840113e768
Marius Renner <ragingcactus@ragingcactus.net>
2021-12-21 12:25:09 +0100

    BM40: Add right meta key for easier Meta+Number shortcuts
    For example Windows+1 in Windows, or Meta+1 in i3wm.

--- keyboards/kprepublic/bm40hsrgb/keymaps/RagingCactus/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/keymaps/RagingCactus/keymap.c
@@ -42,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI |  GUI | Alt  | Lower|    Space    |Raise|AltGr/-|   =  |   [  |   ]  |
+ * | Ctrl |  GUI |  GUI | Alt  | Lower|    Space    |Raise|AltGr/-|Win/= |   [  |   ]  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_mit( \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_BSPC, \
-  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT), \
-  KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, LOWER,      KC_SPC,       RAISE,  RALT_T(KC_MINS), KC_EQL,  KC_LBRC, RCTL_T(KC_RBRC) \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,            KC_BSPC, \
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,   KC_G,    KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN,         KC_QUOT, \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH,         RSFT_T(KC_ENT), \
+  KC_LCTL, KC_LGUI, KC_LGUI, KC_LALT, LOWER,      KC_SPC,       RAISE,  RALT_T(KC_MINS), RGUI_T(KC_EQL),  KC_LBRC, RCTL_T(KC_RBRC) \
 ),
 
 /* Arrow
@@ -97,14 +97,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   1  |   2  |   3  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      0      |      |   -  |   0  | KP_+ |      |
+ * |      |      |      |      |      |      0      |      |   -  |      | KP_+ |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_mit( \
-   KC_DEL,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7, KC_8,    KC_9,      KC_0,   _______, \
+   KC_DEL,   KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7, KC_8,    KC_9,     KC_0,    _______, \
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,     KC_4, KC_5,    KC_6,     KC_DOT,  KC_ASTR, \
   _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,   KC_F12,    KC_1, KC_2,    KC_3,     KC_SLSH, _______, \
-  _______, _______, _______, _______, _______,          KC_0,     _______, KC_MINS, KC_0, _______, KC_PLUS  \
+  _______, _______, _______, _______, _______,          KC_0,     _______, KC_MINS, _______,  _______, KC_PLUS  \
 ),
 
 /* Raise
