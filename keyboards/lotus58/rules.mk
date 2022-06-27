3bba143fca93e9bd668fce73c30230a9b1d7f185
Marius Renner <ragingcactus@ragingcactus.net>
2022-06-27 21:15:08 +0200

    Lotus58: Add clarifying comment to bootloader

--- keyboards/lotus58/rules.mk
+++ keyboards/lotus58/rules.mk
@@ -3,7 +3,7 @@ MCU = atmega32u4
 
 # Bootloader selection
 #BOOTLOADER = caterina # default for pro micro
-BOOTLOADER = qmk-dfu
+BOOTLOADER = qmk-dfu # Requires you to reflash the bootloader using an ISP programmer (another arduino can serve that purpose)
 
 ## Options ##
 BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
