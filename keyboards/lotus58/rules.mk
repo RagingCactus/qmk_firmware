848f9d4be2dd4870db797fdb7c33bbef950dcba1
Marius Renner <ragingcactus@ragingcactus.net>
2022-07-10 18:49:05 +0200

    Lotus58: Add RP2040 based variant
    Using a Pro Micro compatible RP2040 MCU board such as the Adafruit
    KB2040 seems to work great and offers a LOT more firmware space to play
    with. I have also verified that the Pro Micro variant of the firmware
    still works.

--- keyboards/lotus58/rules.mk
+++ keyboards/lotus58/rules.mk
@@ -31,8 +31,3 @@ TAP_DANCE_ENABLE = yes
 # Debounce eagerly (report change immediately) to keep latency low
 # See https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/#eagerdebounce
 DEBOUNCE_TYPE = sym_eager_pk
-
-# DEBUG
-# CONSOLE_ENABLE = yes
-
-DEFAULT_FOLDER = lotus58/promicro
