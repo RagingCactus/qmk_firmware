a59d137fe642f3dbd72bfc996c688c3945ec7396
Marius Renner <ragingcactus@ragingcactus.net>
2022-07-12 18:14:23 +0200

    Lotus58: Move enter key to right alt instead of shift
    It was way too easy to accidentally press enter when it was on the shift
    key. It's way better on the right alt key, which I don't press nearly as
    often.

--- keyboards/lotus58/keymaps/ragingcactus/keymap.c
+++ keyboards/lotus58/keymaps/ragingcactus/keymap.c
@@ -225,7 +225,7 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
  * | Ctrl |   Z  |   X  |   C  |   V  |   B  |GUI/Win|    |       |   N  |   M  |   ,  |   .  |   /  | Ctrl  |
  * `-----------------------------------------|-------/    \-------|------------------------------------------'
  *                    | Alt  | Shift | Space / LOWER /     \ Raise \ | Shift|AltGr | Win  |
- *                    | TDALT|       /     |       /        \       \|      |      | Gui  |
+ *                    | TDALT|       /     |       /        \       \|      |Enter | Gui  |
  *                    `-------------------'-------'          '-------'--------------------'
  */
 
@@ -236,7 +236,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,  KC_Q,    KC_W,      KC_E,              KC_R,    KC_T,                             KC_Y,    KC_U,      KC_I,    KC_O,     KC_P,     KC_BSPC,
    KC_TAB,  KC_A,    KC_S,      KC_D,              KC_F,    KC_G,                             KC_H,    KC_J,      KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
    KC_LCTL, KC_Z,    KC_X,      KC_C,              KC_V,    KC_B, TD(TD_GUI_LOWEREDGUI),          XXXXXXX,  KC_N,    KC_M,      KC_COMM, KC_DOT,   KC_SLSH,  KC_RCTL,
-                XXXXXXX, TD(TD_ALT_LOWEREDALT), KC_LSFT, KC_SPC, LOWER,          RAISE   , RSFT_T(KC_ENT), KC_RALT,   TD(TD_GUI_LOWEREDGUI),   XXXXXXX
+                XXXXXXX, TD(TD_ALT_LOWEREDALT), KC_LSFT, KC_SPC, LOWER,          RAISE   , KC_RSFT, RALT_T(KC_ENT),   TD(TD_GUI_LOWEREDGUI),   XXXXXXX
 ),
 
 [_GAME] = LAYOUT(
@@ -244,7 +244,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC,  KC_Q,    KC_W,      KC_E,              KC_R,    KC_T,                             KC_Y,    KC_U,      KC_I,    KC_O,     KC_P,     KC_BSPC,
    KC_TAB,  KC_A,    KC_S,      KC_D,              KC_F,    KC_G,                             KC_H,    KC_J,      KC_K,    KC_L,     KC_SCLN,  KC_QUOT,
    KC_LCTL, KC_Z,    KC_X,      KC_C,              KC_V,    KC_B, KC_LGUI,          XXXXXXX,  KC_N,    KC_M,      KC_COMM, KC_DOT,   KC_SLSH,  KC_RCTL,
-                XXXXXXX, KC_LALT, KC_LSFT, KC_SPC, LOWERGAME,                       RAISE   , KC_ENT, KC_RALT,   KC_RGUI,   XXXXXXX
+                XXXXXXX, KC_LALT, KC_LSFT, KC_SPC, LOWERGAME,                       RAISE   , KC_RSFT, KC_ENT,   KC_RGUI,   XXXXXXX
 ),
 
 [_LOWER] = LAYOUT(
