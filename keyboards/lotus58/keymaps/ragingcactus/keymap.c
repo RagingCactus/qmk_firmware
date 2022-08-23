940ebbca6ec4077530853cda85ad46c6926e8a9c
Marius Renner <ragingcactus@ragingcactus.net>
2022-08-23 10:36:20 +0200

    Lotus58: Move curly braces to right half

--- keyboards/lotus58/keymaps/ragingcactus/keymap.c
+++ keyboards/lotus58/keymaps/ragingcactus/keymap.c
@@ -265,7 +265,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT(
   _______, _______, _______,  _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
-  _______, _______, KC_LCBR,  KC_RCBR, KC_EQL, _______,                          _______, KC_MINS, KC_LPRN, KC_RPRN, _______, KC_DEL,
+  _______, _______, KC_LCBR,  KC_RCBR, KC_EQL, _______,                          KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, _______, KC_DEL,
   KC_DEL, KC_PAUSE, _______,  KC_PGUP, KC_HOME, KC_RBRC,                         KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_PIPE, KC_GRV,
   _______, KC_PSCR, KC_PAST,  KC_PGDN,  KC_END,  KC_EQL, KC_APP,       _______, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, _______,
                      _______,  _______, _______, KC_INS, _______,       _______, _______, _______, _______, _______
