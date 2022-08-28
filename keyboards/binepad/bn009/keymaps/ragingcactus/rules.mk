28b80a4bf4ddb059cc98dc2abceb5340aac21495
Marius Renner <ragingcactus@ragingcactus.net>
2022-08-28 13:37:31 +0200

    BN009: Add basic BN009 keymap with sym_eager_pk debouncing

---
+++ keyboards/binepad/bn009/keymaps/ragingcactus/rules.mk
@@ -0,0 +1,3 @@
+# Debounce eagerly (report change immediately) to keep latency low
+# See https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/#eagerdebounce
+DEBOUNCE_TYPE = sym_eager_pk
