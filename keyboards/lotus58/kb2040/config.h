b3667d69d52c375637cdf974370a2f3dd68168d6
Marius Renner <ragingcactus@ragingcactus.net>
2022-09-10 13:59:19 +0200

    Lotus58: Use USB_VBUS_PIN to detect master (requires custom circuit)

--- keyboards/lotus58/kb2040/config.h
+++ keyboards/lotus58/kb2040/config.h
@@ -28,12 +28,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define SOFT_SERIAL_PIN GP1
 
-// #define USB_VBUS_PIN GP12
+// Use EITHER USB_VBUS_PIN OR the auto detect below!
+// Chances are you don't have a USB_VBUS_PIN on your KB2040, unless you do what
+// I did and build a small circuit that provides 0V if VBUS is not present and
+// ~3.3V when it is present and provide that signal to GP12 on the KB2040 by
+// using the JST connector. If you did not build that circuit, this won't work!
+#define USB_VBUS_PIN GP12
 // OR
 // Detects USB and assigns master (Not recommended on Elite C/Pro Micro mix)
-#define SPLIT_USB_DETECT
-#define SPLIT_USB_TIMEOUT 2000
-#define SPLIT_USB_TIMEOUT_POLL 10
+// #define SPLIT_USB_DETECT
+// #define SPLIT_USB_TIMEOUT 2000
+// #define SPLIT_USB_TIMEOUT_POLL 10
 
 #define RGB_DI_PIN GP0
 
