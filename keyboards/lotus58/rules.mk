4999b32b5a03b7e1e680d352d90f086fee1a6232
Marius Renner <ragingcactus@ragingcactus.net>
2022-09-12 15:06:48 +0200

    Lotus58: Specify promicro variant as default
    This prevents build errors when not specifying a variant, for example
    when using the following command:
    
    qmk compile -kb lotus58 -km ragingcactus
    
    This will now build the promicro variant by default.
    
    Use one of the following commands to specifically build a variant:
    
    qmk compile -kb lotus58/promicro -km ragingcactus
    qmk compile -kb lotus58/kb2040 -km ragingcactus

--- keyboards/lotus58/rules.mk
+++ keyboards/lotus58/rules.mk
@@ -31,3 +31,5 @@ TAP_DANCE_ENABLE = yes
 # Debounce eagerly (report change immediately) to keep latency low
 # See https://michael.stapelberg.ch/posts/2021-05-08-keyboard-input-latency-qmk-kinesis/#eagerdebounce
 DEBOUNCE_TYPE = sym_eager_pk
+
+DEFAULT_FOLDER = lotus58/promicro
