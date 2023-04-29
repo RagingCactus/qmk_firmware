53b2adc4e5cae8ab3f3131122ec82ed999992709
Marius Renner <ragingcactus@ragingcactus.net>
2023-04-29 16:31:00 +0200

    BN009: Update Binepad keymap

--- keyboards/binepad/bn009/keymaps/ragingcactus/keymap.c
+++ keyboards/binepad/bn009/keymaps/ragingcactus/keymap.c
@@ -18,7 +18,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
-  KC_7,    KC_8,    KC_9,
+  KC_ESC,  KC_F2,    KC_9,
   KC_4,    KC_5,    KC_6,
   KC_1,    KC_2,    KC_3
 )
